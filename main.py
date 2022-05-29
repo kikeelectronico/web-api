fe17064b594908f4a99ef770d5349a7612810987
enrique.gomez <enrique.gomez@bq.com>
2022-05-29 15:29:08 +0200

    set cors

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=['*.enriquegomez.me'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
